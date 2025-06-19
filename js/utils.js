5de1338f0d83308ee945c60efa2228552b6b0769
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-06-19 17:39:27 +0530

    some little changes

--- js/utils.js
+++ js/utils.js
@@ -7,7 +7,8 @@ const formattedDate = now.toLocaleDateString('en-US', options);
 dateElement.textContent = formattedDate;
 
 // viday play/play functionality
-const video = document.getElementById("myVideo"); const icon = document.getElementById("playPauseIcon");
+const video = document.getElementById("myVideo");
+const icon = document.getElementById("playPauseIcon");
 function togglePlayPause() {
   if (video.paused) {
     video.play();
