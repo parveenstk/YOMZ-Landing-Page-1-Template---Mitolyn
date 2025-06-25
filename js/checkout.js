9d953fc97e260e17fc0ee83f1a16865bfb81748f
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-06-25 13:11:24 +0530

    cart details updation are done.

--- js/checkout.js
+++ js/checkout.js
@@ -106,9 +106,8 @@ updateTimer(); // initial display
 const timerInterval = setInterval(updateTimer, 1000);
 
 // Products Data
-const packs = {
+const packsPrice = {
     '1-person': {
-        imageHeading: '1 Person - 1 Bag',
         normallyPrice: '79.99',
         couponPercentage: '40%',
         couponPrice: '32',
@@ -116,7 +115,6 @@ const packs = {
     },
 
     '2-people': {
-        imageHeading: '2 People - 2 Bags',
         normallyPrice: '159.98',
         couponPercentage: '43%',
         couponPrice: '68.78',
@@ -124,7 +122,6 @@ const packs = {
     },
 
     '3-people': {
-        imageHeading: '3 People - 3 Bags',
         normallyPrice: '239.97',
         couponPercentage: '46%',
         couponPrice: '110.39',
@@ -132,72 +129,127 @@ const packs = {
     },
 };
 
+const upperCartData = {
+    '1-person': {
+        bagName: 'bag-1 Img',
+        imagePath: '/images/bags/desktop-bag-1.png',
+        imageHeading: '1 Person - 1 Bag',
+    },
+
+    '2-people': {
+        bagName: 'bag-2 Img',
+        imagePath: '/images/bags/desktop-bag-2.png',
+        imageHeading: '2 People - 2 Bags',
+    },
+
+    '3-people': {
+        bagName: 'bag-3 Img',
+        imagePath: '/images/bags/desktop-bag-3.png',
+        imageHeading: '3 People - 3 Bags',
+    },
+};
+
 // Get query string from URL
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const productId = urlParams.get('product');
-console.log("productId:", productId);
+// console.log("productId:", productId);
 
 // Update pack display
 const updatedPack = (productId) => {
-    const product = packs[productId];
+    const product = packsPrice[productId];
+    const uprCart = upperCartData[productId];
     const totalPrice = parseFloat(product.todayPrice) + 19.95 + 32.17;
+
+    if (uprCart) {
+        const updatedUprCart = `
+                    <div class="upper-cart">
+                        <div class="products-image">
+                            <img src=${uprCart.imagePath} class="img-fluid" alt="${uprCart.bagName}">
+                        </div>
+                        <div class="summery-data text-center">
+                            <p class="mb-0"><strong>Cart Summary</strong></p>
+                            <h4 class="mb-0"><strong>${uprCart.imageHeading}</strong></h4>
+                            <p style="text-align:center">
+                                <span style="background-color:rgba( 15 , 128 , 0 , 1 );color:rgba( 255 , 255 , 255 , 1 );font-family:'roboto' , sans-serif;font-size:14px">
+                                    <strong>&nbsp;4.5&nbsp;</strong>
+                                </span>
+                                <span
+                                    style="background-color:rgba( 255 , 255 , 255 , 0.01 );font-family:'roboto' , sans-serif;font-size:14px"><strong></strong></span>
+                                <span
+                                    style="background-color:rgba( 255 , 255 , 255 , 0.01 );color:rgba( 15 , 128 , 0 , 1 );font-family:'roboto' , sans-serif;font-size:14px">
+                                    <strong>Excellent!</strong>
+                                </span>
+                                <span style="font-family:'roboto' , sans-serif;font-size:14px"></span>
+                                <span
+                                    style="color:rgba( 132 , 132 , 132 , 1 );font-family:'roboto' , sans-serif;font-size:14px">
+                                    (2,797 reviews)
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                            `
+
+        // Update the HTML (upperCartData)
+        const cart = document.querySelectorAll('.upper-cart');
+        cart.forEach(element => {
+            element.innerHTML = updatedUprCart;
+        });
+    };
 
     if (product) {
         const updatedPrice = `
-                                        <div class="com-font normal">
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>Normally</span>
-                                                        <span><del class="strike-red">$${product.normallyPrice}</del></span>
-                                                    </p>
-                                                </div>
-                                                <div class="com-font coupon">
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>Coupon: <span
-                                                                class="off-color">${product.couponPercentage}<u></u></span></span>
-                                                        <span class="off-color">$${product.couponPrice}</span>
-                                                    </p>
-                                                </div>
-                                                <hr>
-                                                <div class="total-price">
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>Today's Price</span>
-                                                        <span>$${product.todayPrice}</span>
-                                                    </p>
-                                                </div>
+                    <div class="com-font normal">
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>Normally</span>
+                                    <span><del class="strike-red">$${product.normallyPrice}</del></span>
+                                </p>
+                            </div>
+                            <div class="com-font coupon">
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>Coupon: <span
+                                            class="off-color">${product.couponPercentage}<u></u></span></span>
+                                    <span class="off-color">$${product.couponPrice}</span>
+                                </p>
+                            </div>
+                            <hr>
+                            <div class="total-price">
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>Today's Price</span>
+                                    <span>$${product.todayPrice}</span>
+                                </p>
+                            </div>
 
-                                                <div class="cart-checkbox manage-checkbox">
-                                                    <input id="cart-checkbox" class="cursor" type="checkbox">
-                                                    <label for="cart-checkbox">
-                                                        I do not want to receive exclusive content and promotion form
-                                                        this vendor, and do not share my personal information.
-                                                    </label>
-                                                </div>
+                            <div class="cart-checkbox manage-checkbox">
+                                <input id="cart-checkbox" class="cursor" type="checkbox">
+                                <label for="cart-checkbox">
+                                    I do not want to receive exclusive content and promotion form
+                                    this vendor, and do not share my personal information.
+                                </label>
+                            </div>
 
-                                                <!-- Desktop - Cart Prices -->
-                                                <div class="subtotal mt-3">
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>Subtotal</span>
-                                                        <span>$${product.todayPrice}</span>
-                                                    </p>
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>Shipping & Handling</span>
-                                                        <span>$19.95</span>
-                                                    </p>
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>Tax</span>
-                                                        <span>$32.17</span>
-                                                    </p>
-                                                    <p class="mb-0 d-flex justify-content-between">
-                                                        <span>TOTAL</span>
-                                                        <span>$${totalPrice}</span>
-                                                    </p>
-                                        </div>`;
+                            <!-- Desktop - Cart Prices -->
+                            <div class="subtotal mt-3">
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>Subtotal</span>
+                                    <span>$${product.todayPrice}</span>
+                                </p>
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>Shipping & Handling</span>
+                                    <span>$19.95</span>
+                                </p>
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>Tax</span>
+                                    <span>$32.17</span>
+                                </p>
+                                <p class="mb-0 d-flex justify-content-between">
+                                    <span>TOTAL</span>
+                                    <span>$${totalPrice}</span>
+                                </p>
+                    </div>`;
 
-        // Update the HTML
+        // Update the HTML (packPrices)
         const priceContainers = document.querySelectorAll('.product-prices');
-        console.log("priceContainer:", priceContainers);
-
         priceContainers.forEach(container => {
             container.innerHTML = updatedPrice;
         });
