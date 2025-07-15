53c074ad25aac6ecd53eacf3abd938d79c42df7c
parveenstk <parveen.kumar@suretekinfosoft.com>
2025-07-15 11:55:52 +0530

    done.

--- offer1/js/offer1.js
+++ offer1/js/offer1.js
@@ -1,33 +1,28 @@
-let timeLeft = 3 * 60; // 180 seconds (3 minutes)
+const popUp = document.getElementById('timer-popUp');
 
-const timerDisplay = document.getElementById('timer-display');
+// timer function
+const runTimer = () => {
+    let timeLeft = 3 * 60; // 180 seconds (3 minutes)
+    const timerDisplay = document.getElementById('timer-display');
+    const timerBox = document.getElementById('timer-box');
 
-const timerBox = document.getElementById('timer-box');
+    function updateTimer() {
+        const minutes = String(Math.floor(timeLeft / 60)).padStart(2, '0');
+        const seconds = String(timeLeft % 60).padStart(2, '0');
+        // timerDisplay.textContent = `${minutes}:${seconds}`;
 
-function updateTimer() {
-
-    const minutes = String(Math.floor(timeLeft / 60)).padStart(2, '0');
-
-    const seconds = String(timeLeft % 60).padStart(2, '0');
-
-    // timerDisplay.textContent = `${minutes}:${seconds}`;
-
-    timerDisplay.innerHTML = `<span id="timer-text">Offer expires in :</span> ${minutes}:${seconds}`;
-
-    if (timeLeft <= 0) {
-
-        clearInterval(timerInterval);
-
-        timerDisplay.innerHTML = `<span id="timer-text">Offer Expired !</span>`
-
-        // timerBox.style.display = 'none'; // or any other action
-
+        timerDisplay.innerHTML = `<span id="timer-text">Offer expires in :</span> ${minutes}:${seconds}`;
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            timerDisplay.innerHTML = `<span id="timer-text">Offer Expired !</span>`
+            // timerBox.style.display = 'none'; // or any other action
+        }
+        timeLeft--;
     }
 
-    timeLeft--;
-
+    updateTimer(); // initial display
+    const timerInterval = setInterval(updateTimer, 1000);
 }
 
-updateTimer(); // initial display
-
-const timerInterval = setInterval(updateTimer, 1000);
+// onClick timer starts
+popUp ? popUp.addEventListener('click', runTimer) : console.log("Please define 'popUp'.");
